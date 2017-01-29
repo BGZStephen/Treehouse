0696adaa2cbdc88513809ce976b5c1fbce3ff89a
BGZStephen <sjw948@gmail.com>
2017-01-29 21:09:25 +0000

    simple js app - SHOUT

---
+++ javascript basics/js app - SHOUT/scripts.js
@@ -0,0 +1,8 @@
+// var passphrase = "Open Sesame";
+// console.log(passphrase.length);
+// console.log(passphrase.toLowerCase());
+// console.log(passphrase);
+// console.log(passphrase.toUpperCase());
+
+var stringToShout = prompt("Enter the message you want to shout:")
+alert(stringToShout.toUpperCase() + "!!!!!!");
