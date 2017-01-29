b59d9656f89a98cc23c29636d7c96d097f1764ad
BGZStephen <sjw948@gmail.com>
2017-01-29 21:17:52 +0000

    js app - story teller

---
+++ javascript basics/js app - story maker/story.js
@@ -0,0 +1,6 @@
+// Capture 3 user inputs, concatenate them and output them as a story
+
+var userName = prompt("Please enter your name");
+var userAnimal = prompt("Please provide a type of animal");
+var userColor = prompt("Please provide a color");
+document.write("My name is " + userName + " I have a pet " + userAnimal + " who is " + userColor);
