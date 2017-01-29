3c5e3b766d2babbd0733f950ada5b92909b058ed
BGZStephen <sjw948@gmail.com>
2017-01-29 20:53:20 +0000

    javascript variable concatenation

---
+++ javascript basics/concatenation/scripts.js
@@ -0,0 +1,4 @@
+var name = prompt("What is your name?");
+var message = "Hello " + name;
+message += ", welcome to my JavaScripts basics code! I hope you enjoy the code snippets";
+document.write(message);
