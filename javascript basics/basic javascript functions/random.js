63f5f66ed0b02655dac38a1db7de0e897d9b5335
BGZStephen <sjw948@gmail.com>
2017-02-01 07:32:07 +0000

    passing parameters to functions

--- javascript basics/basic javascript functions/random.js
+++ javascript basics/basic javascript functions/random.js
@@ -1,8 +1,20 @@
-function getRandomNumber() {
-    var randomNumber = Math.floor( Math.random() * 6 ) + 1;
+function getRandomNumber(upper) {
+    var randomNumber = Math.floor( Math.random() * upper ) + 1;
     return randomNumber;
 }
 
-alert(getRandomNumber);
-console.log(getRandomNumber);
-document.write(getRandomNumber);
+// console.log(getRandomNumber(6));
+// console.log(getRandomNumber(12));
+// console.log(getRandomNumber(10));
+
+// return outputds can be displayed in multiple ways
+// alert(getRandomNumber);
+// console.log(getRandomNumber);
+// document.write(getRandomNumber);
+
+function getArea(x, y, unit) {
+  var area = x * y;
+  return area + " " + unit;
+}
+
+document.write(getArea(4, 4, "sq ft"));
