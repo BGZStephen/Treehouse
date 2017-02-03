be784a734e0474ca6b30dfeb04c13c3a340686e2
BGZStephen <sjw948@gmail.com>
2017-02-03 22:13:05 +0000

    refactor challenge
    additional functions to reduce code usage

--- javascript - loops arrays objects/refactor challenge/js/script.js
+++ javascript - loops arrays objects/refactor challenge/js/script.js
@@ -4,15 +4,20 @@ var green;
 var blue;
 var rgbColor;
 
-function randColorValue() {
+function randRGB() {
   return Math.floor(Math.random() * 256 );
 }
 
+function randColorValue() {
+  var color = "rgb(";
+  color += randRGB() + ", ";
+  color += randRGB() + ", ";
+  color += randRGB() + ")";
+  return color;
+}
+
 for (var i = 1; i <= 10; i += 1) {
-  red = randColorValue();
-  green = randColorValue();
-  blue = randColorValue();
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  rgbColor = randColorValue();
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
 
