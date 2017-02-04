fe8cc18de4cba3e48b57a2f4f9956bae16875bcf
BGZStephen <sjw948@gmail.com>
2017-02-04 08:01:47 +0000

    array basics

---
+++ javascript - loops arrays objects/array basics/js/shopping.js
@@ -0,0 +1,8 @@
+var shoppingList = [
+  "carrots",
+  "Eggs",
+  "Milk",
+];
+document.write(shoppingList[0] + "<br>");
+document.write(shoppingList[1] + "<br>");
+document.write(shoppingList[2] + "<br>");
