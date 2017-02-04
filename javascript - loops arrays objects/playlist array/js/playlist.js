8b43c4c9d8597333706a5b781978181c0901964f
BGZStephen <sjw948@gmail.com>
2017-02-04 08:35:06 +0000

    for loops with arrays
    use a for loop to loop through an array and print its values

---
+++ javascript - loops arrays objects/playlist array/js/playlist.js
@@ -0,0 +1,7 @@
+var playList = [ ];
+playList.push("I Did It My Way");
+playList.push("Respect", "Imagine");
+playList.unshift("Born To Run");
+playList.unshift("Louie, Louie", "Welcome To The Jungle");
+
+printList(playList);
