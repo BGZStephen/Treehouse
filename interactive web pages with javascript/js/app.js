7e951402479f5e1fac2e44437492b2e9690ff1e1
BGZStephen <sjw948@gmail.com>
2017-02-07 19:17:51 +0000

    to do list application
    created function to assign event handlers to corresponding elements, for
    loop to cycle through each list item and execute function.

--- interactive web pages with javascript/js/app.js
+++ interactive web pages with javascript/js/app.js
@@ -1,3 +1,7 @@
+// 1. created functions
+// 2. created variables to hold elements we'll be binding event handlers to
+// 3. created a function to cycle through both completd and incompleted ul's and bind event handlers to the corresponding elements
+
 // problem: no current user interatction
 // solution: add interactivity to elements with javascript
 
@@ -51,5 +55,27 @@ var taskIncomplete = function() {
   // check box is unchecked, append to incomplete task ul
 }
 
+var bindTaskEvents = function(taskListItem, checkboxEventHandler) {
+  console.log("Bind List Item")
+  // for each list item, select it's children
+  var checkbox = taskListItem.querySelector("input[type='checkbox']");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+  // bind the edit task to the edit button
+  editButton.onclick = editTask;
+  // bind delete task to delete button
+  deleteButton.onclick = deleteTask;
+  // bind checkBoxEventHandler to checkbox
+  checkbox.onchange = checkboxEventHandler;
+}
+
 // set the click handler to the addtask button
 addButton.onclick = addTask;
+
+for (var i = 0; i< incompleteTasksHolder.children.length; i++) {
+  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+
+for (var i = 0; i< completedTasksHolder.children.length; i++) {
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
